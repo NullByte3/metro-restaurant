39a393fd6b4fb0146a6d589350483460110dc5d9
vivsam <hi@coolkidz.me>
2025-03-31 13:28:52 +0300

    WIP: Renamed the stylesheet, fixed the runtime engine.

--- runtime.js
+++ runtime.js
@@ -1,8 +1,8 @@
 // ENDPOINT IS:  "https://media2.edu.metropolia.fi/restaurant/";
 
-function getRestaurants() {
+async function getRestaurants() {
     let endpoint = "https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants";
-    let res =  fetch(endpoint);
+    let res =  await fetch(endpoint);
     if (!res.ok) {
         console.error("Error fetching restaurants:", res.statusText);
     }
